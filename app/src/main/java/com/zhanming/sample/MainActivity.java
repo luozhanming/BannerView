aa8ac25ce459cf7e24816312fd34a839d80aecab
罗展鸣 <459557210@qq.com>
2017-07-27 21:29:34 +0800

    更新Demo

--- app/src/main/java/com/zhanming/sample/MainActivity.java
+++ app/src/main/java/com/zhanming/sample/MainActivity.java
@@ -42,7 +42,5 @@ public class MainActivity extends AppCompatActivity {
             //    .setIndicatorActiveColor(Color.BLUE)
            //     .setIndicatorInactiveColor(Color.RED)
                 .initialize();
-
-
     }
 }
