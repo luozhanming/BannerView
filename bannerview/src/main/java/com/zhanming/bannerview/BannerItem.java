b7711d2c2ae87add52b1daec6e068b31040c615b
罗展鸣 <459557210@qq.com>
2017-07-30 19:42:58 +0800

    改点错误

--- bannerview/src/main/java/com/zhanming/bannerview/BannerItem.java
+++ bannerview/src/main/java/com/zhanming/bannerview/BannerItem.java
@@ -13,7 +13,7 @@ public class BannerItem<T> {
     private View item;
     private int drawable = -1;
     private String imgUrl;
-    private BannerAction<T> mAction;
+    private BannerAction mAction;
 
 
     public BannerItem(View item, @Nullable BannerAction action,@Nullable T content) {
